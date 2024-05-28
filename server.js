cd15300bdd28932dbc68dde57fc15e98b99bf8c5
Namo <rishabh01345@gmail.com>
2024-05-29 00:29:43 +0530

    second commit

--- server.js
+++ server.js
@@ -10,6 +10,11 @@ const server=http.createServer(app);
 
 const io=new Server(server);
 
+app.use(express.static('build'))
+
+app.use((req,res,next)=>{
+    res.sendFile(path.join(__dirname,'build','index.html'));
+})
 //create a map to store socket id map with username
 const userSocketMap={};
 
